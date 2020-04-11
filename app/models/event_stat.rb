a81ddd8e88300992a4a2cb76b4ce71a1ceaf0092
pepe <pepe@pop-os.localdomain>
2020-04-11 13:55:30 -0400

    ¨Callbacks¨

--- app/models/event_stat.rb
+++ app/models/event_stat.rb
@@ -1,3 +1,11 @@
 class EventStat < ApplicationRecord
   belongs_to :event
+  before_save do
+    self.attendance = update!
+    self.sales = update!
+  end
+  before_destroy do
+    self.attendance = update!
+    self.sales = update!
+  end
 end
