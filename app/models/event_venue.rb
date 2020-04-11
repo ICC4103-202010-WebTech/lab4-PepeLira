06bed03962f37457fc8a9c88c0a609519f4088a7
pepe <pepe@pop-os.localdomain>
2020-04-11 13:32:58 -0400

    ¨Validates¨

--- app/models/event_venue.rb
+++ app/models/event_venue.rb
@@ -1,3 +1,6 @@
 class EventVenue < ApplicationRecord
   has_many :events
+  validates :name, :address, presence: true
+  validates :capacity, numericality: { only_integer: true }
+  validates :capacity, length: { minimum: 10 }
 end
